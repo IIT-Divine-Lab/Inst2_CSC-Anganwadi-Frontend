5b27503eafe595da83b5a8ac9600ab6ff05e3c7e
kushdeepwalia <skushdeep5@gmail.com>
2024-12-05 18:32:51 +0530

    inst 2 api added

--- src/apiUrl.js
+++ src/apiUrl.js
@@ -1,3 +1,3 @@
-const apiUrl = process.env.NODE_ENV === "development" ? 'http://' + process.env.REACT_APP_SERVER_IP + ':5000/api/v1/' : 'https://cscanganwadi-c6be713ed90b.herokuapp.com/api/v1/';
+const apiUrl = process.env.NODE_ENV === "development" ? 'http://' + process.env.REACT_APP_SERVER_IP + ':5000/api/v1/' : 'https://api.anganwaditest.co.in/api/v1/';
 
 export default apiUrl;
